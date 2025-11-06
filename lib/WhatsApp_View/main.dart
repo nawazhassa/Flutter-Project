46f88a901a1881a506cf8d8950b250fc1dbd6e50
Hasan_nawaz <Hassannawaz1390@gmail.com>
2025-11-06 16:27:00 +0500

    whatsapp view

--- lib/WhatsApp_View/main.dart
+++ lib/WhatsApp_View/main.dart
@@ -15,9 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          fontFamily: 'MomoTrustDisplay-Regular'
-      ),
+      theme: ThemeData(fontFamily: 'MomoTrustDisplay-Regular'),
       home: DefaultTabController(
         length: 4,
         child: Scaffold(
@@ -26,41 +24,65 @@ class _MyAppState extends State<MyApp> {
             foregroundColor: Colors.white,
             title: Text('WhatApp'),
             centerTitle: false,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  print('search is pressed');
+                },
+                icon: Icon(Icons.search),
+              ),
+              PopupMenuButton(
+                icon: Icon(Icons.more_vert),
+                itemBuilder: (context) => [
+                  PopupMenuItem(child: Text('New Group')),
+                  PopupMenuItem(child: Text('New Broadcast')),
+                ],
+              ),
+            ],
+
             bottom: TabBar(
               labelColor: Colors.white,
               unselectedLabelColor: Colors.white.withOpacity(0.8),
               indicatorColor: Colors.white,
               tabs: [
-                Tab(icon: Icon(Icons.camera_alt),),
+                Tab(icon: Icon(Icons.camera_alt)),
                 Tab(text: 'Chats'),
-                Tab(text: 'Status',),
-                Tab(text: 'Calls',),
+                Tab(text: 'Status'),
+                Tab(text: 'Calls'),
               ],
             ),
           ),
 
-          body: TabBarView(children: [
-            // Text('camera'),
-            Center(child: Text('home', style: TextStyle(color: Colors.red),),),
-            Center(
-                child: ListView(
-                  children: [
-                    ListView.builder(itemBuilder: (context, index) {
-                      return ListTile(
-                          leading: CircleAvatar(
-                            radius: 5,
-                            backgroundImage: NetworkImage('https://images.pexels.com/photos/33539853/pexels-photo-33539853.jpeg'),)
-                          );
-
-
-                      },)
-                  ],
-                )
-
-            ),
-            Center(child: Text('logout', style: TextStyle(color: Colors.red),)),
-            Center(child: Text('Calls', style: TextStyle(color: Colors.red),))
-          ]),
+          body: TabBarView(
+            children: [
+              // Text('camera'),
+              Center(
+                child: Text('home', style: TextStyle(color: Colors.red)),
+              ),
+              ListView.builder(
+                itemCount: 200,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: NetworkImage(
+                        'https://images.pexels.com/photos/18858931/pexels-photo-18858931.jpeg',
+                      ),
+                    ),
+                    title: Text('Hassan Nawaz'),
+                    subtitle: Text('JinFreshup'),
+                    trailing: Text('05:10'),
+                  );
+                },
+              ),
+              Center(
+                child: Text('logout', style: TextStyle(color: Colors.red)),
+              ),
+              Center(
+                child: Text('Calls', style: TextStyle(color: Colors.red)),
+              ),
+            ],
+          ),
 
           // body: TabBarView(children: children),
         ),
@@ -68,4 +90,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
